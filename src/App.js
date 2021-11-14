fbb99942999cc1fd08ac1e8fe04d954518ab6340
Ziaad-khaled <ziadkhaled1918@gmail.com>
2021-11-14 17:35:28 +0100

    app.js is edited

---
+++ src/App.js
@@ -0,0 +1,80 @@
+
+// External variables
+const express = require("express");
+const mongoose = require('mongoose');
+const path = require('path');
+// THIS IS WRONG NEVER DO THAT !! Only for the task we put the DB Link here!! NEVER DO THAAAT AGAIN !!
+const MongoURI = 'mongodb://Ziad:z@cluster0-shard-00-00.izp8e.mongodb.net:27017,cluster0-shard-00-01.izp8e.mongodb.net:27017,cluster0-shard-00-02.izp8e.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-jkas6k-shard-0&authSource=admin&retryWrites=true&w=majority' ;
+
+
+//App variables
+const app = express();
+const port = process.env.PORT || "8000";
+const User = require('./models/User');
+app.use(express.static('public'))
+// #Importing the userController
+
+
+// configurations
+// Mongo DB
+mongoose.connect(MongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(result =>console.log("MongoDB is now connected") )
+.catch(err => console.log(err));
+
+/*
+                                                    Start of your code
+*/
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.get('/createflight', function(req, res) {
+  res.sendFile(path.join(__dirname, '/public/createFlight.html'));
+});
+
+app.get("/Home", (req, res) => {
+  res.status(200).send("You have everything installed !");
+});
+
+app.get('/styles.css', function(req, res) {
+  res.sendFile(__dirname , '/css/styles.css');
+});
+
+
+app.get("/jobTA",(req,res)=>{
+  User.find({job:"T3"}).then((result)=>{
+   res.send(result);
+  })});
+
+  app.get("/TAS",(req,res)=>{
+    User.find({Job:'Student'}).exec(function(err,users){
+      res.send(users)
+    });
+    });
+    app.get("/student",async(req,res)=>{
+      const newStudent =new User({
+        Name:"Ziad",
+        Email:'ziad',
+        Age:"21",
+        BornIn:"Egypt",
+        LivesIn:"Berlin",
+        MartialStatus:"Single",
+        PhoneNumber:"012345",
+        Job:"Student"
+      });
+        await newStudent.save(newStudent)
+        res.send(newStudent)
+    });
+// #Routing to usercontroller here
+
+
+
+
+/*
+                                                    End of your code
+*/
+
+// Starting server
+app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+  });
