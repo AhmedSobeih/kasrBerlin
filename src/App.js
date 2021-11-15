c8d77de080174789c9155eca847be3136e490da8
marwankhalid9876 <75695160+marwankhalid9876@users.noreply.github.com>
2021-11-15 19:19:28 +0100

    User Database added, began server side code

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ const MongoURI = 'mongodb://Ziad:z@cluster0-shard-00-00.izp8e.mongodb.net:27017,
 const app = express();
 const port = process.env.PORT || "8000";
 const Flight = require('./Models/Flight');
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(express.urlencoded({extended:false}));
 // #Importing the userController
 
 
@@ -40,30 +41,110 @@ app.get('/styles.css', function(req, res) {
   res.sendFile(__dirname , '/css/styles.css');
 });
 
+app.get('/login', (req,res)=>{
+    res.render('/login');
+})
+
+app.get('/searchFlight', (req,res)=>{
+  res.render('/searchFlight');
+})
+app.get('/flight/:number', (req,res)=>{
+      
+  Flight.find({Number : req.params.number})
+  .then((flights)=>{
+    res.render('result', {searchResult: flights});
+}).catch((err) =>  res.render('result', null))
+    
+});
+
+app.get('/allFlights', (req,res)=>{
+  Flight.find({})
+  .then((flights)=>{
+    res.render('result', {searchResult: flights});
+}).catch((err) =>  res.render('result', null))
+    
+});
 
 
-  app.get("/flight",(req,res)=>{
-    Flight.find({Number : 55}).exec(function(err,Flight){
-      res.send(Flight)
+app.post('login',(req,res) =>{
+  Users.find({username:req.body.username, password:req.body.password})
+  .then((user)=>{
+      if(user==null)
+      {
+          //if user not found in database render the same login page with usertype null
+          res.render('/', {userType: null})
+          return;
+      }
+      //we need to create a session
+      res.render('AdminHomePage', {userType: 'Admin', name: user.name});
+  }).catch((err) => res.render('/', {userType: 'error'}));//if an error happened while accessing db, return string error
+})
+
+
+
+
+
+  app.post("/searchFlight",(req,res)=>{
+    const searchCriteria = req.body.searchCriteria;
+    const searchValue = req.body.searchValue;
+    if(searchCriteria == null)
+    {
+      res.redirect('allFlights');
+    }  
+    Flight.find({searchCriteria : searchValue})
+    .then((flights)=>{
+      res.render('result', {searchResult: flights});
+  }).catch((err) =>  res.render('result', null))
     });
-    });
-    app.get("/student",async(req,res)=>{
+
+    //For creating a flight
+    app.post("/flight",async(req,res)=>{
       const newFlight =new Flight({
-        Number:55,
-        flightDate : "<2001-05-20>",
-        DepatureTime:"<2001-05-20>",
-        ArrivalTime:"<2001-05-20>",
-        EconomySeats:20,
-        BusinessSeats:20,
-        DepatureAirport:"BER",
-        ArrivalAirport:"BAR"
+        Number:req.body.number,
+        flightDate : req.body.date,
+        DepatureTime: req.body.depatureTime,
+        ArrivalTime: req.body.arrivalTime,
+        EconomySeats: req.body.economySeats,
+        BusinessSeats: req.body.businessSeats ,
+        DepatureAirport: req.body.depatureAirport,
+        ArrivalAirport: req.body.arrivalAirport
       });
-        await newFlight.save(newFlight)
-        res.send(newFlight)
+      try{
+        await newFlight.save(newFlight);
+        res.redirect(`/flight/${newFlight.Number}`);
+      }
+      catch(err)
+      {
+        console.log(err);//error must be sent to user
+      }
+    });
+
+    app.put('/flight/:number', (req,res)=>{
+      const flightNumber = req.body.params;
+      const filter = {Number: flightNumber};
+      const criteria = req.body.criteria;
+      const value = req.body.value; 
+      const update = {criteria : value};
+      Flight.findOneAndUpdate(filter, update, {
+        new: true,
+      })
+      .then((flight)=>{
+        res.redirect(`/flight/${flightNumber}`);//if successful, render the flight again with the new values
+    }).catch((err) =>  res.send(err))
+        
+    });
+
+    app.delete('/flight/:number', (req,res)=>{
+      const flightNumber = req.body.params;
+      const filter = {Number: flightNumber};
+    
+      Flight.findOneAndRemove(filter)
+      .then((flight)=>{
+        res.redirect('/AdminHomePage');//if successful, redirect to the home page
+    }).catch((err) =>  res.send(err))
+        
     });
 // #Routing to usercontroller here
-
-
 
 
 /*
---
+++ src/Models/Users.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+//first name, last name, home address, country code, telephone number(s), email and passport number.
+
+const UserSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  homeAddress: {
+    type: String,
+    required: true,
+  },
+  telephoneNumbers: {
+    type: [String],
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  passportNumber: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+}, { timestamps: true });
+
+const User = mongoose.model('User', flightSchema);
+module.exports = User;
