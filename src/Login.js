8f396f6fb9f3453d555c8d2a7ad22ad47376871d
AhmedSobeih <ahmedsobeih2018@gmail.com>
2021-11-24 13:46:02 +0100

    modified index and login

--- src/Login.js
+++ src/Login.js
@@ -25,7 +25,11 @@ export default function Login() {
         })
 }
  
+function tryRegister(){
 
+          navigate('/register');
+
+}
 function tryLogin (){
   // const data = { username, password}
   var bodyFormData = new FormData();
@@ -65,33 +69,18 @@ function handlePassword(text){
               <div className="rounded-t mb-0 px-6 py-6">
                 <div className="text-center mb-3">
                   <h6 className="text-blueGray-500 text-sm font-bold">
-                    Sign in with
+                    Sign Up With
                   </h6>
                 </div>
                 <div className="btn-wrapper text-center">
-                  <button
-                    className="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
-                    type="button"
-                  >
-                    <img
-                      alt="..."
-                      className="w-5 mr-1"
-                      src={require("assets/img/github.svg").default}
-                    />
-                    Github
-                  </button>
-                  <button
-                    className="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
-                    type="button"
-                  >
-                    <img
-                      alt="..."
-                      className="w-5 mr-1"
-                  src={require("assets/img/google.svg").default}
-
-                    />
-                    Google
-                  </button>
+                 <div className="text-center mt-6">
+                    <button
+                      className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
+                      type="button" onClick={tryRegister}
+                    >
+                      Sign up
+                    </button>
+                  </div>
                 </div>
                 <hr className="mt-6 border-b-1 border-blueGray-300" />
               </div>
--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './css/styles.css';
-
-import AdminHome from 'AdminHome';
-import ViewFlights from 'ViewFlights';
 import App from './App.js';
+
+
 const Anchor =({title})=>{
         return (
             <li className="nav-item">
@@ -83,9 +82,9 @@ function Home(){
       <div>
            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container px-5">
-                <a className="navbar-brand" href="#!">Start Bootstrap</a>
+                <a className="navbar-brand" href="/register">Register now</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
-                <div className="collapse navbar-collapse" id="navلآbbarSupportedContent">
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li className="nav-item"><a className="nav-link active" aria-current="page" href="#!">Home</a></li>
                          <Anchor title="about"/>
@@ -101,10 +100,10 @@ function Home(){
                 <div class="row gx-5 justify-content-center">
                     <div class="col-lg-6">
                         <div class="text-center my-5">
-                            <h1 class="display-5 fw-bolder text-white mb-2">Present your business in a whole new way</h1>
-                            <p class="lead text-white-50 mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit!</p>
+                            <h1 class="display-5 fw-bolder text-white mb-2">Airo Airline Homepage</h1>
+                            <p class="lead text-white-50 mb-4">The gratest adventure lies ahead</p>
                             <div class="d-grid gap-3 d-sm-flex justify-content-sm-center">
-                                <a class="btn btn-primary btn-lg px-4 me-sm-3" href="#features">Get Started</a>
+                                <a class="btn btn-primary btn-lg px-4 me-sm-3" href="/Login">Login</a>
                                 <a class="btn btn-outline-light btn-lg px-4" href="#!">Learn More</a>
                             </div>
                         </div>
@@ -131,14 +130,14 @@ function Home(){
         <section className="bg-light py-5 border-bottom">
             <div className="container px-5 my-5">
                 <div className="text-center mb-5">
-                    <h2 className="fw-bolder">Pay as you grow</h2>
+                    <h2 className="fw-bolder">Choose your own class</h2>
                     <p className="lead mb-0">With our no hassle pricing plans</p>
                 </div>
                 <div className="row gx-5 justify-content-center">
                    
-                    <Subscriptions plan ="free" perValue="Mo." price="$0" first="5GB storage" second="Unlimited public projects" third="Community access" fourth="Unlimited private projects"/>
-                    <Subscriptions plan ="free" perValue="Mo." price="$0" first="5GB storage" second="Unlimited public projects" third="Community access" fourth="Unlimited private projects"/>
-                    <Subscriptions plan ="free" perValue="Mo." price="$0" first="5GB storage" second="Unlimited public projects" third="Community access" fourth="Unlimited private projects"/>
+                    <Subscriptions plan ="free" perValue=" " price="$0" first="Economy class" second="Seats usually recline" third="have a fold-down table" fourth="Seat pitch ranges from 28 to 36 inche"/>
+                    <Subscriptions plan ="free" perValue=" " price="$0" first="Business class" second="A seat with direct aisle access and no seatmate" third="An excellent pillow and blanket" fourth="Wifi, ideally reasonably priced"/>
+                    <Subscriptions plan ="free" perValue=" " price="$0" first="First class" second="seat will typically be extremely comfortable and spacious" third="seat will likely fully recline into a bed" fourth="have its own enclosed pod around it for added privacy"/>
 
                 </div>
             </div>
@@ -148,7 +147,7 @@ function Home(){
             <div className="container px-5 my-5 px-5">
                 <div className="text-center mb-5">
                     <h2 className="fw-bolder">Customer testimonials</h2>
-                    <p className="lead mb-0">Our customers love working with us</p>
+                    <p className="lead mb-0">Our loyal customers who love travelling with us</p>
                 </div>
                 <div className="row gx-5 justify-content-center">
                     <div className="col-lg-6">
@@ -158,8 +157,8 @@ function Home(){
                                 <div className="d-flex">
                                     <div className="flex-shrink-0"><i className="bi bi-chat-right-quote-fill text-primary fs-1"></i></div>
                                     <div className="ms-4">
-                                        <p className="mb-1">Thank you for putting together such a great product. We loved working with you and the whole team, and we will be recommending you to others!</p>
-                                        <div className="small text-muted">- Client Name, Location</div>
+                                        <p className="mb-1">A big Thanks to @AiroAirlines for excellent customer service on my last trip (20 hours of travel). It is much appreciated!</p>
+                                        <div className="small text-muted">- Ahmed, Berlin</div>
                                     </div>
                                 </div>
                             </div>
@@ -170,8 +169,8 @@ function Home(){
                                 <div className="d-flex">
                                     <div className="flex-shrink-0"><i className="bi bi-chat-right-quote-fill text-primary fs-1"></i></div>
                                     <div className="ms-4">
-                                        <p className="mb-1">The whole team was a huge help with putting things together for our company and brand. We will be hiring them again in the near future for additional work!</p>
-                                        <div className="small text-muted">- Client Name, Location</div>
+                                        <p className="mb-1">@AiroAirlines is the best airline for customer service by far. No hassle flight changes, best points rewards program, and best employees. There's a reason I only fly AA. Shoutout to the amazing crew at CAI airport too- always a pleasure there. </p>
+                                        <div className="small text-muted">- Linda, Egypt</div>
                                     </div>
                                 </div>
                             </div>
@@ -185,8 +184,8 @@ function Home(){
             <div className="container px-5 my-5 px-5">
                 <div className="text-center mb-5">
                     <div className="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i className="bi bi-envelope"></i></div>
-                    <h2 className="fw-bolder">Get in touch</h2>
-                    <p className="lead mb-0">We'd love to hear from you</p>
+                    <h2 className="fw-bolder">Subscribe to our mailbox</h2>
+                    <p className="lead mb-0">We'd love for you to get the latest updates</p>
                 </div>
                 <div className="row gx-5 justify-content-center">
                     <div className="col-lg-6">
