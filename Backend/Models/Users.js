01d94ed7bdc50d71cbe8eab22a73e16afaf57406
Ziaad-khaled <ziadkhaled1918@gmail.com>
2021-12-12 22:47:52 +0100

    edit

--- Backend/Models/Users.js
+++ Backend/Models/Users.js
@@ -48,7 +48,7 @@ const UserSchema = new Schema({
     type:[Number],
     required:true 
   },
-  //["E",[110,112,113], price]
+  //["Economy",[110,112,113], price]
   // Economy   seats   price
   //Or business
   flightsReservedDetails:{
