219c27dcf3a10445d7b24a8b9a1ce8347325e6bd
AhmedSobeih <ahmedsobeih2018@gmail.com>
2022-01-17 21:15:30 +0100

    mixing merges

--- Backend/App.js
+++ Backend/App.js
@@ -148,11 +148,7 @@ app.post('/login', async (req, res) => {
 })
 
 function generateAccessToken(user) {
-<<<<<<< HEAD
   return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15555555555s' })
-=======
-  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15555555s' })
->>>>>>> b49618e480bfcb56b72b4c322a5349c11c060bbc
 }
 
 ////authServer
